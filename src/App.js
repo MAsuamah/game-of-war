c654c0a27c071d7aa5cc6809fac0eae389a95945
Michelle <michey.asmah21@gmail.com>
2021-12-18 18:52:29 -0500

    added parameters to war function

--- src/App.js
+++ src/App.js
@@ -5,10 +5,13 @@ function App() {
 
   const [cpuDeck, setCpuDeck] = useState([])
   const [playerDeck, setPlayerDeck] = useState([])
+
   const [cpuWar, setCpuWar] = useState([])
   const [playerWar, setPlayerWar] = useState([])
-  const [drawnPlCard, setDrawnPlCard] = useState({})
-  const [drawnCpCard, setDrawnCpCard] = useState({})
+
+  const [drawnPlCard, setDrawnPlCard] = useState(null)
+  const [drawnCpCard, setDrawnCpCard] = useState(null)
+  
   const [deckId, setDeckId] = useState('')
 
   useEffect(() => {
@@ -74,9 +77,6 @@ function App() {
 
   const drawCards = () => {
 
-    setDrawnPlCard({})
-    setDrawnCpCard({})
-
     const pDeck = playerDeck.slice()
     const cDeck = cpuDeck.slice()
 
@@ -92,52 +92,51 @@ function App() {
     if (parseInt(drawnPlayerCard.value) > parseInt(drawnCpuCard.value)) {
       setPlayerDeck(pDeck.slice(1).concat([drawnPlayerCard, drawnCpuCard]))
       setCpuDeck(cDeck.slice(1))
-      setDrawnPlCard({})
-      setDrawnCpCard({})
+      setDrawnPlCard(null)
+      setDrawnCpCard(null)
     }
 
     else if (parseInt(drawnPlayerCard.value) < parseInt(drawnCpuCard.value)) {
       setCpuDeck(cDeck.slice(1).concat([drawnPlayerCard, drawnCpuCard]))
       setPlayerDeck(pDeck.slice(1))
-      setDrawnPlCard({})
-      setDrawnCpCard({})
+      setDrawnPlCard(null)
+      setDrawnCpCard(null)
     }
 
     else if (parseInt(drawnPlayerCard.value) === parseInt(drawnCpuCard.value)){
-      war()
+      war(pDeck, cDeck)
     }
   }
 
-  const war = () => {
-    const pDeck = playerDeck.slice()
-    const cDeck = cpuDeck.slice()
+  const war = (ppDeck, ccDeck) => {
+
     const cWarDeck = cpuWar.slice()
     const pWarDeck = playerWar.slice()
 
-    const pWar = pDeck.slice(1).slice(0,2)
-    const cWar = cDeck.slice(1).slice(0,2)
+    const pWar = ppDeck.slice(1).slice(0,2)
+    const cWar = ccDeck.slice(1).slice(0,2)
 
     if (parseInt(pWar[pWar.length-1].value) > parseInt(cWar[cWar.length-1].value)) {
-      setPlayerDeck(pDeck.slice(3, pDeck.length).concat([drawnPlCard, drawnCpCard, ...pWar, ...cWar, ...cWarDeck, ...pWarDeck]))
-      setCpuDeck(cDeck.slice(3, cDeck.length))
+      setPlayerDeck(ppDeck.slice(3, ppDeck.length).concat([drawnPlCard, drawnCpCard, ...pWar, ...cWar, ...cWarDeck, ...pWarDeck]))
+      setCpuDeck(ccDeck.slice(3, ccDeck.length))
       setCpuWar([])
       setPlayerWar([])
-      setDrawnPlCard({})
-      setDrawnCpCard({})
+      setDrawnPlCard(null)
+      setDrawnCpCard(null)
     }
 
     else if (parseInt(pWar[pWar.length-1].value) < parseInt(cWar[cWar.length-1].value)) {
-      setCpuDeck(cDeck.slice(3, cDeck.length).concat([drawnPlCard, drawnCpCard, ...pWar, ...cWar, ...pWarDeck, ...cWarDeck]))
-      setPlayerDeck(pDeck.slice(3, pDeck.length))
+      setCpuDeck(ccDeck.slice(3, ccDeck.length).concat([drawnPlCard, drawnCpCard, ...pWar, ...cWar, ...pWarDeck, ...cWarDeck]))
+      setPlayerDeck(ppDeck.slice(3, ppDeck.length))
       setCpuWar([])
       setPlayerWar([])
-      setDrawnPlCard({})
-      setDrawnCpCard({})
+      setDrawnPlCard(null)
+      setDrawnCpCard(null)
     }
 
     else if (parseInt(pWar[pWar.length-1].value) === parseInt(cWar[cWar.length-1].value)) {
-      setCpuDeck(cDeck.slice(3, cDeck.length))
-      setPlayerDeck(pDeck.slice(3, pDeck.length))
+      setCpuDeck(ccDeck.slice(3, ccDeck.length))
+      setPlayerDeck(ppDeck.slice(3, ppDeck.length))
       setCpuWar([...cpuWar, ...cWar])
       setPlayerWar([...pWar, ...cWar])
       war()
@@ -149,11 +148,24 @@ function App() {
     console.log(cpuDeck)
   }, [playerDeck, cpuDeck])
 
+  
+  useEffect(() => {
+    console.log(playerDeck)
+    console.log(cpuDeck)
+  }, [playerDeck, cpuDeck])
+
 
   return (
     <div>
-      <button onClick={() => startGame()}>Start Game</button>
-      <button onClick={() => drawCards()}>Draw Cards</button>
+      <div>
+        <button onClick={() => startGame()}>Start Game</button>
+        <button onClick={() => drawCards()}>Draw Cards</button>
+      </div>
+      <div>
+        {drawnPlCard && (
+          <img alt="player's drawn card" src={drawnPlCard.image}></img>
+        )}     
+      </div>
     </div>
   );
 }
